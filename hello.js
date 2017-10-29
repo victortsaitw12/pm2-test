b5bc4bc83418871a7aaea3df18f2c1b6c1e12845
victor tsai <cooldan.tw@gmail.com>
2017-10-29 11:47:51 +0000

    reload v1

--- hello.js
+++ hello.js
@@ -1,14 +1,47 @@
+var _ = require('lodash');
+
+var branch = {};
+
+branch.onCENTER_REQ_RELOAD = (proto) => {
+  console.log('onCENTER_REQ_RELOAD');
+  setTimeout(() => {
+    process.exit(1);
+  }, 1000);
+}
+
+branch.onCENTER_REQ_STATE = (proto) => {
+  console.log('onCENTER_REQ_STATE');
+  setTimeout(() => {
+    process.send({
+      type: 'process:msg',
+      data: {
+        proto: 'BRANCH_RSP_STATE',
+        state: 'READY',
+      }
+    });
+  }, 1000);
+};
 
 process.on('message', function(packet){
-  console.log(packet);
+  console.log(JSON.stringify(packet));
+  var protocol = JSON.parse(packet.data);
+  var fn = _.get(branch, 'on' + protocol.proto);
+  console.log(fn);
+  if (fn) return fn(packet);
   process.send({
-      type: 'process:msg',
-      data:{
-          success: true
-      }
+    type: 'process:msg',
+    data:{
+      success: true
+    }
   });    
 });
 
-setInterval(() =>{
-    console.log('hello pm2');
-}, 3000);
+setTimeout(() =>{
+  process.send({
+    type: 'process:msg',
+    data: {
+      proto: 'BRANCH_NTF_READY',
+    }
+  })
+}, 1000)
+
--- index.js
+++ index.js
@@ -3,17 +3,16 @@ var _ = require('lodash');
 
 var center = {
   app_list: [],
+  reload_list: {}
 };
 
-center.sendMsg = function sendMsg(pm_id, packet){
+center.sendMsg = (pm_id, packet) => {
   console.log('sendMsg:' + packet + ' to ' + pm_id);
   pm2.sendDataToProcessId(pm_id, {
     type: 'process:msg',
-    data: {
-      packet: packet,
-    },
+    data: JSON.stringify(packet),
     topic: 'my topic'
-  }, function(err, res){
+  }, (err, res) => {
     if (err){
       return console.log(err);
     }
@@ -22,7 +21,37 @@ center.sendMsg = function sendMsg(pm_id, packet){
 };
 
 center.receiveMsg = (packet) =>{
-  console.log('receiveMsg:' + packet);
+  console.log('receiveMsg:' + JSON.stringify(packet));
+  var fn = _.get(center, 'on' + packet.data.proto);
+  if (fn) {
+    return fn(_.assign(packet.data, {
+      pm_id: packet.process.pm_id, 
+      name: packet.process.name,
+    }))
+  } else {
+    console.log('fn is empty')
+  }
+};
+
+center.online = (packet) => {
+  return console.log('process online:' + packet.process.pm_id);
+};
+
+center.restart = (packet) => {
+  return console.log('process restart:' + packet.process.pm_id);
+};
+
+center.allEvents = (e, packet) => {
+  if (_.eq('online', packet.event)){
+    return center.online(packet);
+  }
+  if(_.eq('exit', packet.event)){
+    return console.log('process exit:' + packet.process.pm_id);
+  }
+  if(_.eq('restart', packet.event)){
+    return center.restart(packet);
+  }
+  console.log(JSON.stringify(packet));
 };
 
 center.build = () => {
@@ -38,24 +67,91 @@ center.build = () => {
       center.app_list = apps;
       pm2.launchBus((err, bus) => {
         if(err) return reject(err);
-        bus.on('process:msg', center.receiveMsg);  
-        resolve();
+        bus.on('process:msg', center.receiveMsg); 
+        bus.on('process:*', center.allEvents);
+        resolve('success');
       });
     });
   });
   });
 };
 
-center.build().then(() => {
-(function callMsg(increm){
-  _.delay(function(){
-    increm += 1;
-    var pm_id = center.app_list[0].pm2_env.pm_id
-    center.sendMsg(pm_id, 'ddd:' + increm);
-    callMsg(increm);
-  }, 2000);
-})(1);
-}).catch((err) =>{
-  console.log(err);
+center.findReloadPM = _.cond([
+  [(reload_list) => {
+    return _.isEmpty(reload_list);
+  }, _.constant(-1)],
+  [(reload_list) => {
+    return _.some(reload_list, (state) => {
+      return _.eq('start', state);
+    });
+  },
+  (reload_list) => {
+    let reload_app = _.pickBy(reload_list, (state) => {
+      return _.eq('start', state);
+    });
+    return _.parseInt(_.keys(reload_app).pop());
+  }],
+  [_.stubTrue, _.constant(-1)],  
+]);
+
+center.reload = _.flow([
+  (reload_list) => {
+    let reload_pm_id = center.findReloadPM(reload_list);
+    if (_.eq(-1, reload_pm_id)){
+      return reload_list;
+    }
+    _.set(reload_list, reload_pm_id, 'reloading');
+    center.sendMsg(_.parseInt(reload_pm_id), {
+      proto: 'CENTER_REQ_RELOAD'
+    });
+    return reload_list;
+  },
+]);
+
+center.onBRANCH_NTF_READY = _.flow([
+  (protocol) => {
+    _.unset(center.reload_list, protocol.pm_id);
+    return center.reload_list;
+  },
+  center.reload,
+  (reload_list) => {
+    center.reload_list = reload_list;
+  },
+]);
+
+center.onDAT_NTF_RELOAD = _.flow([
+  () => {
+    return _.reduce(center.app_list, (res, app) => {
+      return _.set(res, app.pm2_env.pm_id, 'start');
+    }, {});
+  },
+  center.reload,
+  (reload_list) => {
+    center.reload_list = reload_list;
+  },
+]);
+//(function callMsg(increm){
+//  _.delay(function(){
+//    increm += 1;
+//    var pm_id = center.app_list[0].pm2_env.pm_id
+//    center.sendMsg(pm_id, 'ipm2:' + increm);
+//    callMsg(increm);
+//  }, 2000);
+//})(1);
+
+//(function reload(increm){
+//  _.delay(function(){
+//    increm += 1;
+//    var pm_id = center.app_list[0].pm2_env.pm_id;
+//    pm2.restart(pm_id);
+//  }, 2000);
+//})(1);
+
+center.build().then((res) => {
+  return setTimeout(center.onDAT_NTF_RELOAD, 5000);
+}).catch((error) =>{
+  console.log(error);
 });
+
+
 
