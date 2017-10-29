85977a282a13c219db65c3c7fa08bc49a842b8a7
victor tsai <cooldan.tw@gmail.com>
2017-10-29 01:28:55 +0000

    promise build

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var pm2 = require('pm2');
 var _ = require('lodash');
 
-var app_list = [];
+var center = {
+  app_list: [],
+};
 
-function sendMsg(increm){
-  console.log('sendMsg:' + increm + ' to ' + app_list[0].pm2_env.pm_id);
-  pm2.sendDataToProcessId(app_list[0].pm2_env.pm_id, {
+center.sendMsg = function sendMsg(pm_id, packet){
+  console.log('sendMsg:' + packet + ' to ' + pm_id);
+  pm2.sendDataToProcessId(pm_id, {
     type: 'process:msg',
     data: {
-      some: 'data',
-      hello: true,
-      incrm: increm
+      packet: packet,
     },
     topic: 'my topic'
   }, function(err, res){
@@ -18,39 +18,44 @@ function sendMsg(increm){
       return console.log(err);
     }
     return console.log(res);
-  })
+  });
 };
 
-pm2.launchBus(function(err, bus){
-  bus.on('process:msg', function(packet){
-    console.log(JSON.stringify(packet));
-  })    
+center.receiveMsg = (packet) =>{
+  console.log('receiveMsg:' + packet);
+};
+
+center.build = () => {
+  return new Promise((resolve, reject)=>{
+  pm2.connect(true, (err) => {
+    if (err) return reject(err);
+    pm2.start({
+      script: './hello.js',
+      execMode: 'cluster',
+      instances: 2
+    }, (err, apps) => {
+      if(err) return reject(err);
+      center.app_list = apps;
+      pm2.launchBus((err, bus) => {
+        if(err) return reject(err);
+        bus.on('process:msg', center.receiveMsg);  
+        resolve();
+      });
+    });
+  });
+  });
+};
+
+center.build().then(() => {
+(function callMsg(increm){
+  _.delay(function(){
+    increm += 1;
+    var pm_id = center.app_list[0].pm2_env.pm_id
+    center.sendMsg(pm_id, 'ddd:' + increm);
+    callMsg(increm);
+  }, 2000);
+})(1);
+}).catch((err) =>{
+  console.log(err);
 });
 
-pm2.connect(true, function(err){
-    if (err){
-        console.error(err);
-        process.exit(2);
-    }
-    pm2.start({
-        script: './hello.js',
-        execMode: 'cluster',
-        instances: 2
-    }, function(err, apps){
-        if(err){
-            console.log(err);
-            throw err;
-        } 
-        pm2.list((err, list) => {
-            app_list = list;
-           (function callMsg(increm){
-             _.delay(function(){
-             increm += 1;
-              sendMsg(increm);
-              callMsg(increm);
-            }, 2000);
-           })(1);
-        });
-    });
-
-})
